b3504a9d8f0fa5606d67eaab1aa94621e741bb18
Imran Hajiyev <imran.hajiyev@tokeninc.com>
2022-11-04 12:21:36 +0400

    sidebar mobile was refactored for mobile versions; notifications dialog was adjusted for mobile versions; navigation bar dashboard was adjusted for mobile versions;

--- components/sidebar-mobile/index.tsx
+++ components/sidebar-mobile/index.tsx
@@ -1,17 +1,64 @@
 import styles from "../sidebar-mobile/Sidebar.module.css"
-import { useRouter } from "next/router"
+import SidebarItem from "../sidebar/sidebar-item"
+import React from "react"
 
 const SidebarMobile = () => {
-    const router = useRouter()
+    const [collapsed, setCollapsed] = React.useState(true)
+    const [optionsCollapsed, collapseOptions] = React.useState(false)
+
     return (
-        <div className={styles.sidebar}>
-            <div className={styles.header}>
-                <picture className={styles.logo} onClick={() => router.push("/")}>
-                    <source srcSet="/mui-icons/menu-dark.svg" media="(prefers-color-scheme: dark)" />
-                    <img src="/mui-icons/menu.svg" alt="sidebar mobile material icon" width={24} height={24} />
-                </picture>
-            </div>
-        </div>
+        <>
+            {
+                collapsed &&
+                <div className={styles.header} onClick={() => setCollapsed(!collapsed)}>
+                    <picture className={styles.logo} onClick={() => console.log("clicked")}>
+                        <source srcSet="/mui-icons/menu-dark.svg" media="(prefers-color-scheme: dark)" />
+                        <img src="/mui-icons/menu.svg" alt="sidebar mobile material icon" width={24} height={24} />
+                    </picture>
+                </div>
+            }
+
+            {
+                !collapsed &&
+                <div className={styles.sidebar}>
+                    <div className={styles.header} onClick={() => setCollapsed(!collapsed)}>
+                        <picture className={styles.logo} onClick={() => console.log("clicked")}>
+                            <source srcSet="/mui-icons/menu-dark.svg" media="(prefers-color-scheme: dark)" />
+                            <img src="/mui-icons/menu.svg" alt="sidebar mobile material icon" width={24} height={24} />
+                        </picture>
+                    </div>
+                    <div className={styles.content} >
+                        <SidebarItem title="Dashboard" icon="dashboard" important={true} />
+                        <SidebarItem title="Payments" icon="payments" important={true} />
+                        <SidebarItem title="Refunds" icon="refunds" important={true} />
+                        <hr className={styles.divider} />
+                        <SidebarItem
+                            title="Options"
+                            icon="tune"
+                            hasTrailing={true}
+                            important={true}
+                            onClick={() => collapseOptions(!optionsCollapsed)}
+                            state={optionsCollapsed}
+                        />
+                        {
+                            optionsCollapsed &&
+                            <div>
+                                <SidebarItem title="Link" icon="link" important={true} />
+                                <SidebarItem title="Wallet" icon="wallet" important={true} />
+                            </div>
+                        }
+                        <hr className={styles.divider} />
+                        <SidebarItem title="Distribution" icon="distribution" important={true} />
+                    </div>
+                    <div className={styles.footer} >
+                        <SidebarItem title="Profile" icon="profile" />
+                        <SidebarItem title="Settings" icon="settings" />
+                        <SidebarItem title="Sign out" icon="logout" logout={true} />
+                    </div>
+                </div>
+            }
+        </>
+
     )
 }
 
